7a8309aa2f8d2610f255f09b84fe9368739f0882
SadoninS <stanislavsadonin@gmail.com>
2023-06-05 14:51:26 +0400

    oldCangesStackInIDE

--- index.js
+++ index.js
@@ -1,7 +1,31 @@
-const button = document.querySelector("button");
-const input = document.querySelector("input");
-const output = document.querySelector(".output");
+// const button = document.querySelector(".button");
+// const input = document.querySelector("input");
+// const output = document.querySelector(".output");
 
-button.addEventListener("click", function () {
-    output.textContent = input.value;
+// button.addEventListener("click", function () {
+//     output.textContent = input.value;
+// });
+
+// button.addEventListener('click', function() {
+//     output.innerHTML = "Какой ты послушный, молодец!";
+// });
+
+const button = document.querySelector('.button');
+const output = document.querySelector('.output');
+
+button.addEventListener('click', function() {
+    output.innerHTML = "Какой ты послушный, молодец!";
 });
+
+const elements = document.querySelector('.my-skills__item-h2');
+
+ elements.addEventListener('click', function() {
+    if (elements.classList.contains('close')) {
+        elements.classList.add('open');
+        elements.classList.remove('close')
+    } else {
+        elements.classList.add('close')
+        elements.classList.remove('open');
+        
+    }
+})
